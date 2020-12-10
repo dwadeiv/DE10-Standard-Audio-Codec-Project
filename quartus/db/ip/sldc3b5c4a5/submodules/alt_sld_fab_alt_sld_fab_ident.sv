d693b211e3fd0d91a9c204d88e0ec0d80df94207
john-carberry <joca3827@colorado.edu>
2020-12-09 23:58:11 -0700

    added .qar

--- quartus/db/ip/sldc3b5c4a5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ quartus/db/ip/sldc3b5c4a5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 03855cb3ade2d07cbca3
+//   DESIGN_HASH   : ba9f47029970d1e4f7ee
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h03855cb3ade2d07cbca3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hba9f47029970d1e4f7ee };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
